ce39832a09801c0c379d47acb165414f418e9b7b
ramamimu <ahhmaad.blogger@gmail.com>
2022-11-24 15:42:26 +0700

    feat: initial commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello() {
     return this.appService.getHello();
+  }
+
+  @Get('test')
+  getTest() {
+    return this.appService.getTest();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import * as admin from 'firebase-admin';
+import { db } from './firebase/config';
+import { collection, getDocs } from 'firebase/firestore';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello() {
+    let tempChart = [];
+    try {
+      const querySnapshot = await getDocs(collection(db, 'chart'));
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, ' => ', doc.data());
+        const objChart = {
+          id: doc.id,
+          ...doc.data(),
+        };
+        tempChart.push(objChart);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    return tempChart;
+  }
+
+  getTest() {
+    return 'test';
   }
 }
